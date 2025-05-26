f001b4eed6d36bb072fb884c5ba040cc852ebde4
fsyzer0 <46439598+fsyzer0@users.noreply.github.com>
2025-05-26 23:19:24 +0800

    [1][TwoSum] Add Method3
    Change to use malloc, it will reduce time for the memory space init to 0

--- 1_TwoSum/TwoSum1.c
+++ 1_TwoSum/TwoSum1.c
@@ -2,7 +2,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 
-#define METHOD_CHOOSE 1
+#define METHOD_CHOOSE 3
 
 #if METHOD_CHOOSE == 1
 int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
@@ -35,6 +35,28 @@ int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
     *returnSize = 2;
     // I only need to return 2 numbers
     ret = (int*)calloc(2, sizeof(int));
+
+    // use i, j for double for loop
+    for (i=0; i<numsSize; i++){
+        r = target-nums[i];
+        for (j=i+1; j<numsSize; j++){
+            if (r==nums[j]){
+                ret[0]=i;
+                ret[1]=j;
+                return ret;
+            }
+        }
+    }
+    return ret;
+}
+#elif METHOD_CHOOSE == 3
+int* twoSum(int* nums, int numsSize, int target, int* returnSize) {
+    int i, j, r;
+    int *ret;
+
+    *returnSize = 2;
+    // I only need to return 2 numbers
+    ret = (int*)malloc(2*sizeof(int));
 
     // use i, j for double for loop
     for (i=0; i<numsSize; i++){
